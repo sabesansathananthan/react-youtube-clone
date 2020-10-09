f0f66c43fcaee0efc83fe7d0e4d7e581fe802ad2
sabesansathananthan <sabesan.16@cse.mrt.ac.lk>
2020-10-10 05:14:35 +0530

    fix: key

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -6,16 +6,27 @@ const VideoDetail = ({ video }) => {
   const videoURL = `https://www.youtube.com/embed/${video.id.videoId}`;
   return (
     <React.Fragment>
-      <Paper elevation={6} style={{ height: "70%" }}>
+      <Paper elevation={4} style={{ height: "40%", borderRadius: "0.625rem" }}>
         <iframe
           frameBorder="0"
           height="100%"
           width="100%"
           title="Player"
           src={videoURL}
+          style={{
+            borderTopLeftRadius: "0.625rem",
+            borderTopRightRadius: "0.625rem",
+          }}
         />
       </Paper>
-      <Paper elevation={6} style={{ padding: "15px" }}>
+      <Paper
+        elevation={6}
+        style={{
+          padding: "15px",
+          borderBottomLeftRadius: "0.625rem",
+          borderBottomRightRadius: "0.625rem",
+        }}
+      >
         <Typography variant="h4">
           {video.snippet.title} - {video.snippet.channelTitle}
         </Typography>
